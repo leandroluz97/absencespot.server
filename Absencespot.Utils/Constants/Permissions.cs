ccb4c8536a013317d03c3a66f62e54a04593d94d
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-23 00:34:22 +0100

    added utils constant

---
+++ Absencespot.Utils/Constants/Permissions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Utils.Constants
+{
+    public static class Permissions
+    {
+        public const string CREATE = "CREATE";
+        public const string READ = "READ";
+        public const string UPDATE = "UPDATE";
+        public const string DELETE = "DELETE";
+    }
+}
