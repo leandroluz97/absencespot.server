c2d4af948131724f7cf3db6dacfc8e1d59ace538
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-24 22:42:24 +0100

    added provider enums

---
+++ Absencespot.Domain/Enums/ProviderType.cs
@@ -0,0 +1,20 @@
+﻿using Absencespot.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Domain.Enums
+{
+    public class ProviderType : Enumeration
+    {
+        public static readonly ProviderType GOOGLE = new ProviderType(1, "GOOGLE");
+        public static readonly ProviderType MICROSOFT = new ProviderType(2, "MICROSOFT");
+
+        public ProviderType(int id, string name) : base(id, name)
+        {
+
+        }
+    }
+}
--- Absencespot.Domain/Integration.cs
+++ Absencespot.Domain/Integration.cs
@@ -1,4 +1,5 @@
-﻿using Absencespot.Domain.Seedwork;
+﻿using Absencespot.Domain.Enums;
+using Absencespot.Domain.Seedwork;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace Absencespot.Domain
 {
     public class Integration : Entity
     {
-        public string Platform { get; set; }
+        public ProviderType Provider { get; set; }
         public bool IsActive { get; set; }
 
         public int CompanyId { get; set; }
