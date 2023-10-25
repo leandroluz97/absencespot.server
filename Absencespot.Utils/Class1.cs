dbfd1e9619deb802261120958ce58349fdbcedff
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-26 00:07:06 +0100

    added RepositoryOptions utils

--- Absencespot.Utils/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Absencespot.Utils
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Absencespot.Utils/RepositoryOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Absencespot.Utils
+{
+    public class RepositoryOptions
+    {
+        public bool Tracking { get; set; }
+
+        public static RepositoryOptions AsNoTracking()
+        {
+            return new RepositoryOptions(){ Tracking = false};
+        }
+        public static RepositoryOptions AsTracking()
+        {
+            return new RepositoryOptions() { Tracking = true };
+        }
+
+    }
+}
