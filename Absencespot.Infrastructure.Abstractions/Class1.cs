9078df485a4a555d642180e29e04786f45e83df7
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-26 02:11:13 +0100

    added InitOfWork interface with properties and methods

--- Absencespot.Infrastructure.Abstractions/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Absencespot.Infrastructure.Abstractions
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Absencespot.Infrastructure.Abstractions/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using Absencespot.Infrastructure.Abstractions.Repositories;
+
+namespace Absencespot.Infrastructure.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        ICompanyRepository CompanyRepository { get; }
+        IIntegrationRepository IntegrationRepository { get; }
+        ILeaveRepository LeaveRepository { get; }
+        IOfficeRepository OfficeRepository { get; }
+        IRequestRepository RequestRepository { get; }
+        ISettingsRepository SettingsRepository { get; }
+        ITeamRepository ITeamRepository { get; }
+        ITrackRecordRepository TrackRecordRepository { get; }
+        IWorkScheduleRepository WorkScheduleRepository { get; }
+
+        void SaveChanges();
+        void Rollback();
+        Task SaveChangesAsync();
+        Task RollbackAsync();
+    }
+}
