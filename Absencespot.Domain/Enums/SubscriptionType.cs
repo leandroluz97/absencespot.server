576f2e6336ca631e3a7154b1007111bc88965b72
leandroluz97 <leandrodaluz97@gmail.com>
2023-10-28 19:28:50 +0100

    updated enums parameterless contructor

--- Absencespot.Domain/Enums/SubscriptionType.cs
+++ Absencespot.Domain/Enums/SubscriptionType.cs
@@ -12,6 +12,8 @@ namespace Absencespot.Domain.Enums
         public static readonly SubscriptionType FREE = new SubscriptionType(1, "FREE");
         public static readonly SubscriptionType BUSINESS = new SubscriptionType(1, "BUSINESS");
         public static readonly SubscriptionType ENTERPRISE = new SubscriptionType(1, "ENTERPRISE");
+
+        public SubscriptionType(){}
         public SubscriptionType(int id, string name) : base(id, name)
         {
 
