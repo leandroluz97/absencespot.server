9a88614c5f082520fc95b82b40748e90e9ad3f3d
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-01 22:01:14 +0000

    updated IBaseRepository Add and Update methods to return value instead of void

--- Absencespot.Infrastructure.Abstractions/Repositories/IBaseRepository.cs
+++ Absencespot.Infrastructure.Abstractions/Repositories/IBaseRepository.cs
@@ -21,12 +21,12 @@ namespace Absencespot.Infrastructure.Abstractions.Repositories
         T Get(Expression<Func<T, bool>> expression);
         IEnumerable<T> GetAll();
         IEnumerable<T> GetAll(Expression<Func<T, bool>> expression);
-        void Add(T entity);
-        void AddRange(IEnumerable<T> entities);
+        T Add(T entity);
+        IEnumerable<T> AddRange(IEnumerable<T> entities);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entities);
-        void Update(T entity);
-        void UpdateRange(IEnumerable<T> entities);
+        T Update(T entity);
+        IEnumerable<T> UpdateRange(IEnumerable<T> entities);
         Task<T> GetAsync(CancellationToken cancellationToken = default);
         Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
         Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
