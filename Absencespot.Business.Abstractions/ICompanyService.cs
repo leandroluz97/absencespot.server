e5e1e825d29e10c5fe2dd9e1bb7d78949f4ee660
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-07 22:25:32 +0000

    added companyService interface

---
+++ Absencespot.Business.Abstractions/ICompanyService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Absencespot;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface ICompanyService
+    {
+        Task<Dtos.Company> CreateAsync(Guid subscriptionId, Dtos.Company company);
+        Task<Dtos.Company> GetByIdAsync(Guid companyId);
+        Task<Dtos.Company> UpdateAsync(Dtos.Company company);
+        Task DeleteAsync(Guid companyId);
+    }
+}
