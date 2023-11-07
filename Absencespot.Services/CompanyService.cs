a8760b54b3a891ce496b56c75f95f107a677b8c6
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-07 22:21:23 +0000

    added createAsync method

---
+++ Absencespot.Services/CompanyService.cs
@@ -0,0 +1,64 @@
+﻿using Absencespot.Business.Abstractions;
+using Absencespot.Dtos;
+using Absencespot.Infrastructure.Abstractions;
+using Absencespot.Services.Exceptions;
+using Absencespot.Services.Mappers;
+using Absencespot.UnitOfWork;
+using Microsoft.Extensions.Logging;
+
+namespace Absencespot.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly ILogger<CompanyService> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyService(ILogger<CompanyService> logger, IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+        public async Task<Company> CreateAsync(Guid subscriptionId, Dtos.Company companyDto)
+        {
+            if(subscriptionId == default)
+            {
+                throw new ArgumentNullException(nameof(subscriptionId));
+            }
+            if(companyDto == null)
+            {
+                throw new ArgumentNullException(nameof(companyDto));
+            }
+            companyDto.EnsureValidation();
+
+            var subscription = await _unitOfWork.SubscriptionRepository.FindByGlobalIdAsync(subscriptionId);
+            if(subscription == null)
+            {
+                throw new NotFoundException($"Could not find {nameof(subscription)}");
+            }
+
+            var companyDomain = CompanyMapper.ToDomain(companyDto);
+            companyDomain.Subcription = subscription;
+
+            companyDomain = _unitOfWork.CompanyRepository.Add(companyDomain);  
+            await _unitOfWork.SaveChangesAsync();
+            
+            _logger.LogInformation("Created company by Id:");
+
+            return CompanyMapper.ToDto(companyDomain);
+        }
+
+        public Task DeleteAsync(Guid companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> GetByIdAsync(Guid companyId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company> UpdateAsync(Company company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Absencespot.Services/Mappers/CompanyMapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public static class CompanyMapper
+    {
+        public static Domain.Company ToDomain(Dtos.Company dto)
+        {
+            return new Domain.Company()
+            {
+                Name = dto.Name,
+                FiscalNumber = string.IsNullOrWhiteSpace(dto.FiscalNumber) ? null : dto.FiscalNumber,
+            };
+        }
+        public static Dtos.Company ToDto(Domain.Company domain)
+        {
+            return new Dtos.Company()
+            {
+                Id = domain.GlobalId,
+                Name = domain.Name,
+                FiscalNumber = string.IsNullOrWhiteSpace(domain.FiscalNumber) ? null : domain.FiscalNumber,
+            };
+        }
+    }
+}
