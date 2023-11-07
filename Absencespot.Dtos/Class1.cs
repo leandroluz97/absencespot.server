7edd1a221ccf465fb1eb6ca8c8f3cabaeadab75c
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-07 22:25:43 +0000

    added company Dto

--- Absencespot.Dtos/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Absencespot.Dtos
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Absencespot.Dtos/Company.cs
@@ -0,0 +1,16 @@
+﻿namespace Absencespot.Dtos
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? FiscalNumber { get; set; }
+        public void EnsureValidation()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentNullException(nameof(Name));
+            }
+        }
+    }
+}
