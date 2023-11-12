f57363b2389932c899f597cd35d225cbb4338190
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-12 02:15:58 +0000

    added company isActive migration

---
+++ Absencespot.SqlServer/Migrations/20231111223558_added_company_isActive.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Absencespot.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_company_isActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailContact",
+                table: "Company",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Industry",
+                table: "Company",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Company",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailContact",
+                table: "Company");
+
+            migrationBuilder.DropColumn(
+                name: "Industry",
+                table: "Company");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Company");
+        }
+    }
+}
