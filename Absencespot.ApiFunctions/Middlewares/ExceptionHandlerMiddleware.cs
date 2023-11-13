f3a2247e35faa14464412dcc24f3cc87f43d22fb
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-13 23:58:29 +0000

    added GetHttpRequestData function

--- Absencespot.ApiFunctions/Middlewares/ExceptionHandlerMiddleware.cs
+++ Absencespot.ApiFunctions/Middlewares/ExceptionHandlerMiddleware.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.Azure.Functions.Worker;
+﻿using Absencespot.Services.Exceptions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.Functions.Worker.Middleware;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,12 +22,52 @@ namespace Absencespot.ApiFunctions.Middlewares
             }
             catch (Exception ex)
             {
-                var logger = context.GetLogger<ExceptionHandlerMiddleware>();
-                using (logger.BeginScope("Downloading messages"))
-                {
-                    // Scope is "Checking mail" -> "Downloading messages"
-                    //logger.("Connection interrupted");
-                }
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(FunctionContext context, Exception exception)
+        {
+            var httpRequestData = GetHttpRequestData(context);
+
+            var traceId = context.InvocationId;
+            var logger = context.GetLogger<ExceptionHandlerMiddleware>();
+            using (logger.BeginScope("Exception"))
+            {
+                var eventId = new EventId(1001, exception.InnerException?.Message);
+                logger.Log(LogLevel.Error, eventId, exception, exception.Message);
+            }
+
+
+            if(exception is ArgumentNullException || exception is ArgumentNullException)
+            {
+                var httpResponse = httpRequestData.CreateResponse(HttpStatusCode.InternalServerError);
+                // context.StatusCode = (int)statusCode;
+            }
+            if (exception is NotFoundException)
+            {
+
+            }
+            if (exception is InvalidOperationException)
+            {
+
+            }
+
+        }
+
+        public HttpRequestData GetHttpRequestData(FunctionContext functionContext)
+        {
+            try
+            {
+                KeyValuePair<Type, object> keyValuePair = functionContext.Features.SingleOrDefault(f => f.Key.Name == "IFunctionBindingsFeature");
+                object functionBindingsFeature = keyValuePair.Value;
+                Type type = functionBindingsFeature.GetType();
+                var inputData = type.GetProperties().Single(p => p.Name == "InputData").GetValue(functionBindingsFeature) as IReadOnlyDictionary<string, object>;
+                return inputData?.Values.SingleOrDefault(o => o is HttpRequestData) as HttpRequestData;
+            }
+            catch
+            {
+                return null;
             }
         }
     }
