49d0ebfca12bedc52295e82c4d8f49ec445fb0f7
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-15 22:32:45 +0000

    added company dto complementary properties

--- Absencespot.Dtos/Company/Company.cs
+++ Absencespot.Dtos/Company/Company.cs
@@ -3,13 +3,26 @@
     public class Company
     {
         public Guid Id { get; set; }
+        public Guid SubscriptionId { get; set; }
         public string Name { get; set; }
         public string? FiscalNumber { get; set; }
+        public string? EmailContact { get; set; }
+        public string Industry { get; set; }
+        public bool IsActive { get; set; }
+
         public void EnsureValidation()
         {
             if (string.IsNullOrWhiteSpace(Name))
             {
-                throw new ArgumentNullException(nameof(Name));
+                throw new ArgumentException(nameof(Name));
+            }
+            if (string.IsNullOrWhiteSpace(Industry))
+            {
+                throw new ArgumentException(nameof(Industry));
+            }
+            if (SubscriptionId == default)
+            {
+                throw new ArgumentException(nameof(SubscriptionId));
             }
         }
     }
