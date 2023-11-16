b499e7f694882f6999040b682bb14d7a3231b8b2
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-16 00:10:01 +0000

    added IAuthenticationService interface

---
+++ Absencespot.Business.Abstractions/IAuthenticationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface IAuthenticationService
+    {
+        Task Register(Dtos.Register register);
+        Task ConfirmEmail(Dtos.ConfirmEmail confirmEmail);
+        Task<Dtos.LoginResponse> Login(Dtos.LoginRequest login);
+        Task LoginWithGoogle();
+        Task LoginWithMicrosoft();
+        Task LoginWithSlack();
+        Task<Dtos.TokenResponse> ExternalLogin(Dtos.ExternalLogin externalLogin);
+        Task RequestResetPassword(string email);
+        Task ResetPassword(Dtos.ResetPassword resetPassword);
+    }
+}
