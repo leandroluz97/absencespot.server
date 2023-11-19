10642c1ed64dd3b134acf4f78498c51e74701ae2
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-19 20:16:44 +0000

    added register dtos position property

--- Absencespot.Dtos/Register.cs
+++ Absencespot.Dtos/Register.cs
@@ -9,12 +9,17 @@ namespace Absencespot.Dtos
     public class Register
     {
         public string Email { get; set; }
+        public string Position { get; set; }
 
         public void EnsureValidation()
         {
             if (string.IsNullOrWhiteSpace(Email))
             {
                 throw new ArgumentException(nameof(Email));
+            }
+            if (string.IsNullOrWhiteSpace(Position))
+            {
+                throw new ArgumentException(nameof(Position));
             }
         }
     }
