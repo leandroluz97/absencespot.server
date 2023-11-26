22e5bbe6d4d824f0c21be5de1c4c446fc5dd89ca
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-26 23:56:55 +0000

    added office related DTOs

---
+++ Absencespot.Dtos/Absence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class Absence
+    {
+        public double Allowance { get; set; }
+        public double MonthlyAccrual { get; set; }
+        public double MonthCarryOverExpiresAfter { get; set; }
+        public double MonthMaxCarryOver { get; set; }
+    }
+}
---
+++ Absencespot.Dtos/Office.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class Office
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public bool IsEmployeeLeaveStartOnJobStartDate { get; set; }
+        public Address Address { get; set; }
+        public IEnumerable<Guid>? AvailableLeaves { get; set; }
+        public IEnumerable<Absence>? Absences { get; set; }
+
+        public void EnsureValidation()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentException(nameof(Name));
+            }
+            if (Absences == null || !Absences.Any())
+            {
+                throw new ArgumentException(nameof(Absences));
+            }
+            if (AvailableLeaves == null || !AvailableLeaves.Any())
+            {
+                throw new ArgumentException(nameof(AvailableLeaves));
+            }
+        }
+    }
+}
