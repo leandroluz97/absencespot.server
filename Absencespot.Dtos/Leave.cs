aab62c55c9cf8462c9c499835d12b0ba44b53cbc
leandroluz97 <leandrodaluz97@gmail.com>
2023-11-28 01:30:09 +0000

    added leave dtos

---
+++ Absencespot.Dtos/Leave.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class Leave
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Icon { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsApprovalRequired { get; set; }
+        public bool IsReasonRequired { get; set; }
+        public bool IsLimitedQuota { get; set; }
+        public decimal? YearlyQuota { get; set; }
+        public IEnumerable<Guid> OfficeIds { get; set; }
+
+        public void EnsureValidation()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentException(nameof(Name));
+            }
+            if (string.IsNullOrWhiteSpace(Icon))
+            {
+                throw new ArgumentException(nameof(Icon));
+            }
+            if (OfficeIds == null || !OfficeIds.Any())
+            {
+                throw new ArgumentException(nameof(OfficeIds));
+            }
+        }
+    }
+}
