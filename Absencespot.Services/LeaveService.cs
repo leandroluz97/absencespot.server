28d962713cdb0c493fccc675101bda1f0a17d95a
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-05 00:12:45 +0000

    added update office

--- Absencespot.Services/LeaveService.cs
+++ Absencespot.Services/LeaveService.cs
@@ -28,13 +28,51 @@ namespace Absencespot.Services
             {
                 throw new ArgumentNullException(nameof(companyId));
             }
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(
+                companyId,
+                RepositoryOptions.AsTracking(),
+                cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
+            }
             if (leave == null)
             {
                 throw new ArgumentNullException(nameof(leave));
             }
             leave.EnsureValidation();
 
+            var queryable = _unitOfWork.LeaveRepository.AsQueryable(RepositoryOptions.AsNoTracking());
+            queryable = queryable.Where(
+                q => q.Company.GlobalId == companyId &&
+                q.Name.ToLower() == leave.Name.ToLower()
+                );
+
+            var foundLeave = await _unitOfWork.LeaveRepository.FirstOrDefaultAsync(queryable);
+            if (foundLeave != null)
+            {
+                throw new ConflictException(foundLeave.Name);
+            }
             var leaveDomain = LeaveMapper.ToDomain(leave);
+            leaveDomain.Company = companyDomain;
+
+            foreach (var id in leave.OfficeIds)
+            {
+                var office = await _unitOfWork.OfficeRepository.FindByGlobalIdAsync(id, RepositoryOptions.AsTracking());
+                if (office == null)
+                {
+                    throw new NotFoundException(nameof(office));
+                }
+                if (leaveDomain.OfficesLeaves == null)
+                {
+                    leaveDomain.OfficesLeaves = new List<Domain.OfficeLeave>();
+                }
+                leaveDomain.OfficesLeaves.Add(new Domain.OfficeLeave()
+                {
+                    Office = office,
+                    Leave = leaveDomain
+                });
+            }
 
             leaveDomain = _unitOfWork.LeaveRepository.Add(leaveDomain);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
@@ -54,14 +92,25 @@ namespace Absencespot.Services
             {
                 throw new ArgumentNullException(nameof(companyId));
             }
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
+            }
+            //Guid officeId = Guid.Empty;
 
             var queryable = _unitOfWork.LeaveRepository.AsQueryable(RepositoryOptions.AsNoTracking());
             queryable = queryable.Where(l => l.Company.GlobalId == companyId);
+            //queryable = queryable.Where(l => l.Company.GlobalId == companyId && l.OfficesLeaves.Any(of => of.Office.GlobalId == officeId));
 
             var totalLeave = queryable.Count();
 
-            queryable = queryable.Skip(pageNumber * pageSize).Take(pageSize);
+            queryable = queryable.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+            queryable = _unitOfWork.LeaveRepository.Include(queryable, x => x.OfficesLeaves);
+            queryable = _unitOfWork.LeaveRepository.IncludeThen<Domain.OfficeLeave, Domain.Office>(queryable, x => x.Office);
             var leaves = await _unitOfWork.LeaveRepository.ToListAsync(queryable, cancellationToken);
+
+            _logger.LogInformation($"Get leaves pageSize: {pageSize}, pageNumber: {pageNumber}");
 
             return new Pagination<Dtos.Leave>()
             {
@@ -79,16 +128,24 @@ namespace Absencespot.Services
             {
                 throw new ArgumentNullException(nameof(companyId));
             }
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
+            }
+
             if (leaveId == default)
             {
                 throw new ArgumentNullException(nameof(leaveId));
             }
 
             var leaveDomain = await _unitOfWork.LeaveRepository.FindByGlobalIdAsync(leaveId);
-            if(leaveDomain == null)
+            if (leaveDomain == null)
             {
                 throw new NotFoundException(nameof(leaveDomain));
             }
+
+            _logger.LogInformation($"Found leave Id: {leaveId}");
 
             return LeaveMapper.ToDto(leaveDomain);
         }
@@ -98,6 +155,11 @@ namespace Absencespot.Services
             if (companyId == default)
             {
                 throw new ArgumentNullException(nameof(companyId));
+            }
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
             }
             if (leaveId == default)
             {
@@ -109,13 +171,96 @@ namespace Absencespot.Services
             }
             leave.EnsureValidation();
 
-            var leaveDomain = await _unitOfWork.LeaveRepository.FindByGlobalIdAsync(leaveId);
-            if( leaveDomain == null)
+            var queryable = _unitOfWork.LeaveRepository.AsQueryable(RepositoryOptions.AsTracking());
+            queryable = queryable.Where(x => x.Company.GlobalId == companyId && x.GlobalId == leaveId);
+            queryable =  _unitOfWork.LeaveRepository.Include(queryable, x => x.OfficesLeaves);
+            queryable = _unitOfWork.LeaveRepository.IncludeThen<Domain.OfficeLeave, Domain.Office>(queryable, x => x.Office);
+            var leaveDomain = await _unitOfWork.LeaveRepository.FirstOrDefaultAsync(queryable, cancellationToken);
+
+            if (leaveDomain == null)
+            {
+                throw new ArgumentNullException(nameof(leaveDomain));
+            }
+
+            foreach (Domain.OfficeLeave officeLeave in leaveDomain?.OfficesLeaves)
+            {
+                if (!leave.OfficeIds.Any(id => id == officeLeave.Office?.GlobalId))
+                {
+                    leaveDomain.OfficesLeaves.Remove(officeLeave);
+                }
+            }
+
+            foreach (Guid id in leave.OfficeIds)
+            {
+                if (!leaveDomain.OfficesLeaves.Any(of => of.Office.GlobalId == id))
+                {
+                    var office = await _unitOfWork.OfficeRepository.FindByGlobalIdAsync(id, RepositoryOptions.AsTracking());
+                    if (office == null)
+                    {
+                        throw new NotFoundException(nameof(office));
+                    }
+                    leaveDomain.OfficesLeaves ??= new List<Domain.OfficeLeave>();
+                    leaveDomain.OfficesLeaves.Add(new Domain.OfficeLeave
+                    {
+                        Office = office,
+                        Leave = leaveDomain
+                    });
+                }
+            }
+
+            leaveDomain.Name = leave.Name;
+            leaveDomain.Color = leave.Color;
+            leaveDomain.Icon = leave.Icon;
+            leaveDomain.IsActive = leave.IsActive;
+            leaveDomain.IsReasonRequired = leave.IsReasonRequired;
+            leaveDomain.IsLimitedQuota = leave.IsLimitedQuota;
+            leaveDomain.YearlyQuota = leave.YearlyQuota;
+
+            _unitOfWork.LeaveRepository.Update(leaveDomain);
+            await _unitOfWork.SaveChangesAsync();
+
+            _logger.LogInformation($"Updated leave Id: {leaveId}");
+
+            return LeaveMapper.ToDto(leaveDomain);
+        }
+
+
+        public async Task DeleteAsync(Guid companyId, Guid leaveId, CancellationToken cancellationToken = default)
+        {
+            if (companyId == default)
+            {
+                throw new ArgumentNullException(nameof(companyId));
+            }
+
+            var leaveDomain = await LoadById(leaveId, RepositoryOptions.AsNoTracking());
+
+            _unitOfWork.LeaveRepository.Remove(leaveDomain);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            _logger.LogInformation($"Deleted leave Id: {leaveId}");
+        }
+
+        private async Task<Domain.Leave> LoadById(Guid leaveId, RepositoryOptions options, CancellationToken cancellationToken = default)
+        {
+            if (leaveId == default)
+            {
+                throw new ArgumentNullException(nameof(leaveId));
+            }
+
+            Domain.Leave? leaveDomain;
+            if (options == null)
+            {
+                leaveDomain = await _unitOfWork.LeaveRepository.FindByGlobalIdAsync(leaveId, RepositoryOptions.AsNoTracking(), cancellationToken);
+            }
+            else
+            {
+                leaveDomain = await _unitOfWork.LeaveRepository.FindByGlobalIdAsync(leaveId, RepositoryOptions.AsTracking(), cancellationToken);
+            }
+            if (leaveDomain == null)
             {
                 throw new NotFoundException(nameof(leaveDomain));
             }
-
-            return LeaveMapper.ToDto(leaveDomain);
+            return leaveDomain;
         }
     }
 }
--- Absencespot.Services/OfficeService.cs
+++ Absencespot.Services/OfficeService.cs
@@ -23,7 +23,7 @@ namespace Absencespot.Services
             _logger = logger;
             _unitOfWork = unitOfWork;
         }
-        public async  Task<Office> CreateAsync(Guid companyId, Dtos.Office office, CancellationToken cancellationToken = default)
+        public async Task<Dtos.Office> CreateAsync(Guid companyId, Dtos.Office office, CancellationToken cancellationToken = default)
         {
             if (companyId == default)
             {
@@ -35,8 +35,8 @@ namespace Absencespot.Services
             }
             office.EnsureValidation();
 
-            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId);
-            if(companyDomain == null)
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, RepositoryOptions.AsTracking());
+            if (companyDomain == null)
             {
                 throw new NotFoundException($"Not found {companyId}");
             }
@@ -44,23 +44,37 @@ namespace Absencespot.Services
             var officeDomain = OfficeMapper.ToDomain(office);
             officeDomain.Company = companyDomain;
 
-            List<Domain.OfficeLeave> leaves = new List<Domain.OfficeLeave>();
+            List<Domain.OfficeLeave> officeLeaves = new List<Domain.OfficeLeave>();
+            List<Domain.Absence> absences = new List<Domain.Absence>();
             foreach (Guid id in office.AvailableLeaves)
             {
-                var leave = await _unitOfWork.LeaveRepository.FindByGlobalIdAsync(id);
-                if(officeDomain.AvailableLeaves == null)
+                var leave = await _unitOfWork.LeaveRepository.FindByGlobalIdAsync(id, RepositoryOptions.AsTracking());
+                if (leave != null)
                 {
-                    officeDomain.AvailableLeaves = new List<Domain.OfficeLeave>();
-                }
-                if(leave != null)
-                {
-                    officeDomain.AvailableLeaves.Add(new Domain.OfficeLeave()
+                    officeLeaves.Add(new Domain.OfficeLeave()
                     {
                         Leave = leave,
                         Office = officeDomain,
                     });
+
+                    var absence = office.Absences.FirstOrDefault(x => x.LeaveId == leave.GlobalId);
+                    if (absence != null )
+                    {
+                        absences.Add(new Domain.Absence()
+                        {
+                            MonthCarryOverExpiresAfter = absence.MonthCarryOverExpiresAfter,
+                            MonthlyAccrual = absence.MonthlyAccrual,
+                            Allowance = absence.Allowance,
+                            MonthMaxCarryOver = absence.MonthMaxCarryOver,
+                            Leave = leave,
+                            Office = officeDomain,
+                        });
+                    }
                 }
             }
+
+            officeDomain.AvailableLeaves = officeLeaves;
+            officeDomain.Absences = absences;
 
             officeDomain = _unitOfWork.OfficeRepository.Add(officeDomain);
             await _unitOfWork.SaveChangesAsync(cancellationToken);
@@ -69,24 +83,141 @@ namespace Absencespot.Services
             return OfficeMapper.ToDto(officeDomain);
         }
 
-        public Task DeleteAsync(Guid companyId, Guid officeId, CancellationToken cancellationToken = default)
+        public async Task DeleteAsync(Guid companyId, Guid officeId, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            if(companyId == default)
+            {
+                throw new ArgumentNullException(nameof(companyId));
+            }
+            if (officeId == default)
+            {
+                throw new ArgumentNullException(nameof(officeId));
+            }
+
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
+            }
+
+            var officeDomain = await _unitOfWork.OfficeRepository.FindByGlobalIdAsync(officeId);
+            if (officeDomain == null)
+            {
+                throw new NotFoundException(nameof(officeDomain));
+            }
+
+            if(officeDomain.CompanyId != companyDomain.Id)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            _unitOfWork.OfficeRepository.Remove(officeDomain);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
         }
 
-        public Task<Pagination<Office>> GetAllAsync(Guid companyId, Office office, CancellationToken cancellationToken = default)
+        public async Task<Pagination<Office>> GetAllAsync(
+            Guid companyId,
+            int pageNumber = 1,
+            int pageSize = 50,
+            CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            if (companyId == default)
+            {
+                throw new ArgumentNullException(nameof(companyId));
+            }
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
+            }
+            
+            var queryable = _unitOfWork.OfficeRepository.AsQueryable(RepositoryOptions.AsNoTracking());
+            queryable = queryable.Where(l => l.Company.GlobalId == companyId);
+            
+            var totalOffices= queryable.Count();
+            queryable = queryable.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+            queryable = _unitOfWork.OfficeRepository.Include(queryable, x => x.Address);
+            var offices = await _unitOfWork.OfficeRepository.ToListAsync(queryable, cancellationToken);
+
+            _logger.LogInformation($"Get office pageSize: {pageSize}, pageNumber: {pageNumber}");
+
+            return new Pagination<Dtos.Office>()
+            {
+                TotalRecords = totalOffices,
+                TotalPages = (int)Math.Ceiling((decimal)totalOffices / (decimal)pageSize),
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                Items = offices.Select(OfficeMapper.ToDto)
+            };
         }
 
-        public Task<Office> GetByIdAsync(Guid companyId, Office office, CancellationToken cancellationToken = default)
+        public async Task<Dtos.Office> GetByIdAsync(Guid companyId, Guid officeId, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            if (companyId == default)
+            {
+                throw new ArgumentNullException(nameof(companyId));
+            }
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
+            }
+
+            if (officeId == default)
+            {
+                throw new ArgumentNullException(nameof(officeId));
+            }
+
+            var officeDomain = await _unitOfWork.OfficeRepository.FindByGlobalIdAsync(officeId);
+            if (officeDomain == null)
+            {
+                throw new NotFoundException(nameof(officeDomain));
+            }
+
+            _logger.LogInformation($"Found leave Id: {officeId}");
+
+            return OfficeMapper.ToDto(officeDomain);
         }
 
-        public Task<Office> UpdateAsync(Guid companyId, Office office, CancellationToken cancellationToken = default)
+        public async Task<Dtos.Office> UpdateAsync(Guid companyId, Guid officeId, Dtos.Office office, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            if(office == null)
+            {
+                throw new ArgumentNullException(nameof(office));
+            }
+            office.EnsureValidation();
+
+            if (companyId == default)
+            {
+                throw new ArgumentNullException(nameof(companyId));
+            }
+            var companyDomain = await _unitOfWork.CompanyRepository.FindByGlobalIdAsync(companyId, cancellationToken: cancellationToken);
+            if (companyDomain == null)
+            {
+                throw new NotFoundException(nameof(companyDomain));
+            }
+
+            if (officeId == default)
+            {
+                throw new ArgumentNullException(nameof(officeId));
+            }
+            var officeDomain = await _unitOfWork.OfficeRepository.FindByGlobalIdAsync(officeId);
+            if (officeDomain == null)
+            {
+                throw new NotFoundException(nameof(officeDomain));
+            }
+
+            if (officeDomain.CompanyId != companyDomain.Id)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            officeDomain.Name = office.Name;
+            officeDomain.IsEmployeeLeaveStartOnJobStartDate = office.IsEmployeeLeaveStartOnJobStartDate;
+            officeDomain.StartDate = office.StartDate;
+            officeDomain.Address = AddressMapper.ToDomain(office.Address);
+
+            return OfficeMapper.ToDto(officeDomain);
         }
     }
 }
