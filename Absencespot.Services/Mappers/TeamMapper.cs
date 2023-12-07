2e8476438caaa2f19548e0065816794ba2d50e16
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-07 00:38:25 +0000

    added team mapper

---
+++ Absencespot.Services/Mappers/TeamMapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class TeamMapper
+    {
+        public static Domain.Team ToDomain(Dtos.Team dto)
+        {
+            return new Domain.Team()
+            {
+                Name = dto.Name,
+                Description = dto.Description,
+                IsAutoApproved = dto.IsAutoApproved,
+            };
+        }
+
+
+        public static Dtos.Team ToDto(Domain.Team domain)
+        {
+            return new Dtos.Team()
+            {
+                Id = domain.GlobalId,
+                Name = domain.Name,
+                Description = domain.Description,
+                IsAutoApproved = domain.IsAutoApproved,
+                Users = domain.Users?.Select(u => new Dtos.BaseUser()
+                {
+                    Id = u.User.GlobalId,
+                    Email = u.User.Email,
+                    FirstName = u.User.FirstName,
+                    LastName = u.User.LastName,
+                })
+            };
+        }
+    }
+}
