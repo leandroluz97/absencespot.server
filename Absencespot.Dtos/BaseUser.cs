5c322d9c603ed6120a1e6e742152ae3a3560cde5
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-07 00:37:20 +0000

    added team and baseUser data transfer object

---
+++ Absencespot.Dtos/BaseUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class BaseUser
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
+1
---
+++ Absencespot.Dtos/Team.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class Team
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsAutoApproved { get; set; }
+        public IEnumerable<BaseUser>? Users { get; set; }
+
+        public void EnsureValidation()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentException(nameof(Name));
+            }
+        }
+    }
+
+
+}
