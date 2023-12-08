fd08f496ed7a64f3de7a1ddcbb57bf20ab05d0a3
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-08 21:32:17 +0000

    added WorkScheduleFlexibleMapper  and WorkScheduleDefaultMapper

---
+++ Absencespot.Services/Mappers/WorkScheduleDefaultMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class WorkScheduleDefaultMapper
+    {
+        public static Domain.WorkScheduleDefault ToDomain(Dtos.WorkScheduleDefault dto)
+        {
+            return new Domain.WorkScheduleDefault() 
+            {
+                IsDefault = dto.IsDefault,
+                WorkDays = dto.WorkDays,
+                Description = dto.Description,
+                StartHour = dto.StartHour,
+                Name = dto.Name,
+            };
+        }
+        public static Dtos.WorkScheduleDefault ToDto(Domain.WorkScheduleDefault domain)
+        {
+            return new Dtos.WorkScheduleDefault() 
+            { 
+                Id = domain.GlobalId,
+                WorkDays = domain.WorkDays,
+                Name = domain.Name,
+                StartHour = domain.StartHour,
+                Description = domain.Description,
+                IsDefault = domain.IsDefault,
+                EndHour = domain.EndHour,
+            };
+        }
+    }
+}
---
+++ Absencespot.Services/Mappers/WorkScheduleFlexibleMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class WorkScheduleFlexibleMapper
+    {
+        public static Domain.WorkScheduleFlexible ToDomain(Dtos.WorkScheduleFlexible dto)
+        {
+            return new Domain.WorkScheduleFlexible() 
+            { 
+                Name = dto.Name,    
+                Description = dto.Description,
+                Hours = dto.Hours,
+                IsDefault = dto.IsDefault,
+                WorkDays = dto.WorkDays,
+            };
+        }
+        public static Dtos.WorkScheduleFlexible ToDto(Domain.WorkScheduleFlexible domain)
+        {
+            return new Dtos.WorkScheduleFlexible() 
+            { 
+                Id = domain.GlobalId,
+                Name=domain.Name,
+                Description=domain.Description,
+                Hours = domain.Hours,
+                IsDefault = domain.IsDefault,
+                WorkDays = domain.WorkDays,
+            };
+        }
+    }
+}
