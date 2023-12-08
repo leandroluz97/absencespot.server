8c34c6fc35e02c35a43b518090ebd3b12e97dd74
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-08 19:50:46 +0000

    added IWorkscheduleService interface

---
+++ Absencespot.Business.Abstractions/IWorkScheduleService.cs
@@ -0,0 +1,13 @@
+﻿using Absencespot.Utils;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface IWorkScheduleService
+    {
+        Task<Dtos.WorkSchedule> GetByIdAsync(Guid companyId, Guid workScheduleId, CancellationToken cancellationToken = default);
+        Task<Pagination<Dtos.WorkSchedule>> GetAllAsync(Guid companyId, int pageNumber = 1, int pageSize = 50, CancellationToken cancellationToken = default);
+        Task<Dtos.WorkSchedule> CreateAsync(Guid companyId, Dtos.WorkSchedule workSchedule, CancellationToken cancellationToken = default);
+        Task<Dtos.WorkSchedule> UpdateAsync(Guid companyId, Guid workScheduleId, Dtos.WorkSchedule workSchedule, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid companyId, Guid workScheduleId, CancellationToken cancellationToken = default);
+    }
+}
