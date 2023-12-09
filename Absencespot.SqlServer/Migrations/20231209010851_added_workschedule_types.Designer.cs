704353834d844b1743bea05f83e2dec0b572a82d
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-09 01:58:48 +0000

    added workschedule migration types

--- Absencespot.SqlServer/Migrations/20231209010851_added_workschedule_types.Designer.cs
+++ Absencespot.SqlServer/Migrations/20231209010851_added_workschedule_types.Designer.cs
@@ -4,6 +4,7 @@ using Absencespot.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Absencespot.SqlServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231209010851_added_workschedule_types")]
+    partial class added_workschedule_types
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Absencespot.SqlServer/Migrations/20231209010851_added_workschedule_types.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Absencespot.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_workschedule_types : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndHour",
+                table: "WorkSchedule",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Hours",
+                table: "WorkSchedule",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartHour",
+                table: "WorkSchedule",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalWorkDays",
+                table: "WorkSchedule",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkDays",
+                table: "WorkSchedule",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndHour",
+                table: "WorkSchedule");
+
+            migrationBuilder.DropColumn(
+                name: "Hours",
+                table: "WorkSchedule");
+
+            migrationBuilder.DropColumn(
+                name: "StartHour",
+                table: "WorkSchedule");
+
+            migrationBuilder.DropColumn(
+                name: "TotalWorkDays",
+                table: "WorkSchedule");
+
+            migrationBuilder.DropColumn(
+                name: "WorkDays",
+                table: "WorkSchedule");
+        }
+    }
+}
