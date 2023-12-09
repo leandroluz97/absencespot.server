754fb9fb655e35b590290ffdbef659f646cc2998
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-09 21:40:53 +0000

    added RequestService interface

--- Absencespot.Business.Abstractions/IRequestService.cs
+++ Absencespot.Business.Abstractions/IRequestService.cs
@@ -1,7 +1,15 @@
-﻿namespace Absencespot.Business.Abstractions
+﻿using Absencespot.Utils;
+
+namespace Absencespot.Business.Abstractions
 {
     public interface IRequestService
     {
-
+        Task<Dtos.Request> GetByIdAsync(Guid companyId, Guid requestId, CancellationToken cancellationToken = default);
+        Task<Pagination<Dtos.Request>> GetAllAsync(Guid companyId, int pageNumber = 1, int pageSize = 50, CancellationToken cancellationToken = default);
+        Task<Dtos.Request> CreateAsync(Guid companyId, Dtos.Request request, CancellationToken cancellationToken = default);
+        Task<Dtos.Request> UpdateAsync(Guid companyId, Guid requestId, Dtos.Request request, CancellationToken cancellationToken = default);
+        Task<Dtos.Request> ApproveAsync(Guid companyId, Guid requestId, Dtos.ApproveRequest request, CancellationToken cancellationToken = default);
+        Task<Dtos.Request> RejectAsync(Guid companyId, Guid requestId, Dtos.RejectRequest request, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid companyId, Guid requestId, CancellationToken cancellationToken = default);
     }
 }
