bfb71203ffa4f3ad604a0fc64fededfdd92bd5b1
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-11 23:22:09 +0000

    added request mapper

---
+++ Absencespot.Services/Mappers/RequestMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class RequestMapper
+    {
+        public static Domain.Request ToDomain(Dtos.Request dto)
+        {
+            return new Domain.Request() { };
+        }
+        public static Dtos.Request ToDto(Domain.Request domain)
+        {
+            return new Dtos.Request() { };
+        }
+    }
+}
