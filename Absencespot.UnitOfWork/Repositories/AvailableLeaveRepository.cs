724a56d9858de8575941999f90eb47b967f91232
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-19 00:14:26 +0000

    added AvailableLeave repository implementation

---
+++ Absencespot.UnitOfWork/Repositories/AvailableLeaveRepository.cs
@@ -0,0 +1,14 @@
+﻿using Absencespot.Domain;
+using Absencespot.Infrastructure.Abstractions.Repositories;
+using Absencespot.SqlServer;
+
+namespace Absencespot.UnitOfWork.Repositories
+{
+    public class AvailableLeaveRepository : BaseRepository<AvailableLeave>, IAvailableLeaveRepository
+    {
+        public AvailableLeaveRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
