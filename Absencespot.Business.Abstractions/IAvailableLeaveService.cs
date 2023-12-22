f814f27f52488d4f6791f0929336671dc1e515a2
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-22 01:03:19 +0000

    added availableLeave service interface

---
+++ Absencespot.Business.Abstractions/IAvailableLeaveService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface IAvailableLeaveService
+    {
+        Task<IEnumerable<Dtos.AvailableLeave>> GetAllAsync(Guid companyId, Guid userId, CancellationToken cancellationToken = default);
+    }
+}
