8bae226325b910cb16050974afd88a7bad5b4871
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-22 01:04:38 +0000

    added availableLeave mapper

---
+++ Absencespot.Services/Mappers/AvailableLeaveMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class AvailableLeaveMapper
+    {
+        public static Domain.AvailableLeave ToDomain(Dtos.AvailableLeave dto)
+        {
+            return new Domain.AvailableLeave() { };
+        }
+        public static Dtos.AvailableLeave ToDto(Domain.AvailableLeave domain)
+        {
+            return new Dtos.AvailableLeave() { };
+        }
+    }
+}
