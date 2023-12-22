f02ff0306b2c2f0981089d429578df74b5a8a084
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-22 01:05:06 +0000

    added available leave dtos

---
+++ Absencespot.Dtos/AvailableLeave.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class AvailableLeave
+    {
+        public double AvailableDays { get; set; }
+        public DateTime Period { get; set; }
+        public int? AbsenceId { get; set; }
+        public Absence? Absence { get; set; }
+
+    }
+}
