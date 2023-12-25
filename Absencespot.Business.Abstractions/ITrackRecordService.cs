440cfe718d494bf5e8065bf28d1217ea93604703
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-25 22:50:37 +0000

    added track records service interface

---
+++ Absencespot.Business.Abstractions/ITrackRecordService.cs
@@ -0,0 +1,14 @@
+﻿using Absencespot.Utils;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface ITrackRecordService
+    {
+        Task<Dtos.TrackRecordMetrics> GetMetricsByIdAsync(Guid companyId, Guid trackRecordId, CancellationToken cancellationToken = default);
+        Task<Dtos.TrackRecord> GetByIdAsync(Guid companyId, Guid trackRecordId, CancellationToken cancellationToken = default);
+        Task<Pagination<Dtos.TrackRecord>> GetAllAsync(Guid companyId, int pageNumber = 1, int pageSize = 50, CancellationToken cancellationToken = default);
+        Task<Dtos.TrackRecord> CreateAsync(Guid companyId, Dtos.TrackRecord trackRecord, CancellationToken cancellationToken = default);
+        Task<Dtos.TrackRecord> UpdateAsync(Guid companyId, Guid trackRecordId, Dtos.TrackRecord trackRecord, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid companyId, Guid trackRecordId, CancellationToken cancellationToken = default);
+    }
+}
