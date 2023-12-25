45f0587116d7ed9298b5f8278b3520c1a3d6ae92
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-25 22:51:06 +0000

    added track record dtos

---
+++ Absencespot.Dtos/TrackRecordMetrics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class TrackRecordMetrics
+    {
+        public double RequiredMonthlyDays { get; set; }
+        public double TrackTimeCurrentMonth { get; set; }
+        public double OvertimeTrackTimeCurrentMonth { get; set; }
+    }
+}
