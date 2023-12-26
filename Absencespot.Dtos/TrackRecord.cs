9f4b13f473ca41a3b188810882a4dc63a4a91fe2
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-26 23:45:00 +0000

    added trackrecord dtos

--- Absencespot.Dtos/TrackRecord.cs
+++ Absencespot.Dtos/TrackRecord.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Absencespot.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,18 @@ namespace Absencespot.Dtos
 {
     public class TrackRecord
     {
+        public DateTime Date { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public DateTime BreakStart { get; set; }
+        public DateTime BreakEnd { get; set; }
+        public LocationType Location { get; set; }
+        public string Note { get; set; }
+
+
+        public void EnsureValidation()
+        {
+
+        }
     }
 }
