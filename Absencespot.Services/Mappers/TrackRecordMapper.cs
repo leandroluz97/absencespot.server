10c919ba4a24e4df249434e119ebe154af7b093f
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-26 00:07:52 +0000

    added trackRecord mappers

---
+++ Absencespot.Services/Mappers/TrackRecordMapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class TrackRecordMapper
+    {
+        public static Domain.TrackRecord ToDomain(Dtos.TrackRecord dto)
+        {
+            return new Domain.TrackRecord() { };
+        }
+        public static Dtos.TrackRecord ToDto(Domain.TrackRecord domain)
+        {
+            return new Dtos.TrackRecord() { };
+        }
+    }
+}
