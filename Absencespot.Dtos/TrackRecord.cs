879bdce681e1df51c1609e0d942a5405ec0c7149
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-27 01:22:15 +0000

    added ensureValidation guard on TrackRecord DTO

--- Absencespot.Dtos/TrackRecord.cs
+++ Absencespot.Dtos/TrackRecord.cs
@@ -9,18 +9,34 @@ namespace Absencespot.Dtos
 {
     public class TrackRecord
     {
+        public Guid Id { get; set; }
         public DateTime Date { get; set; }
         public DateTime CheckIn { get; set; }
         public DateTime CheckOut { get; set; }
         public DateTime BreakStart { get; set; }
         public DateTime BreakEnd { get; set; }
         public LocationType Location { get; set; }
-        public string Note { get; set; }
+        public string? Note { get; set; }
 
 
         public void EnsureValidation()
         {
-
+            if (CheckOut <= CheckIn)
+            {
+                throw new ArgumentException(nameof(CheckOut));
+            }
+            if (BreakEnd <= BreakStart)
+            {
+                throw new ArgumentException(nameof(BreakEnd));
+            }
+            if (BreakStart <= CheckIn || BreakStart >= CheckOut)
+            {
+                throw new ArgumentException(nameof(BreakStart));
+            }
+            if (BreakEnd <= CheckIn || BreakEnd >= CheckOut)
+            {
+                throw new ArgumentException(nameof(BreakEnd));
+            }
         }
     }
 }
