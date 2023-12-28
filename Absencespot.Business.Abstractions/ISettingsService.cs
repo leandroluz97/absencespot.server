b1f61dd9866c3621319901fa8417ed85189ba760
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-28 22:49:55 +0000

    added ISettingsService interface

---
+++ Absencespot.Business.Abstractions/ISettingsService.cs
@@ -0,0 +1,15 @@
+﻿using Absencespot.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface ISettingsService
+    {
+        Task<Dtos.Settings> GetByIdAsync(Guid companyId, CancellationToken cancellationToken = default);
+        Task<Dtos.Settings> UpdateAsync(Guid companyId, Dtos.Settings settings, CancellationToken cancellationToken = default);
+    }
+}
