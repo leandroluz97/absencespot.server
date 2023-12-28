7a0a03b3363644c944594381610b6aa09db9e26e
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-28 00:46:15 +0000

    added settings dtos

---
+++ Absencespot.Dtos/Settings.cs
@@ -0,0 +1,28 @@
+﻿using Absencespot.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class Settings
+    {
+        public DisplayType DisplayMode { get; set; }
+        public bool PrivacyMode { get; set; }
+        public bool OnBehalfOf { get; set; }
+        public bool IsLoginFromEmailLinkRequired { get; set; }
+        public string DateFormat { get; set; }
+        public string TimeFormat { get; set; }
+        public string TimeZone { get; set; }
+        public string FirstDayOfTheWeek { get; set; }
+        public bool DisplayBirthday { get; set; }
+
+        public void EnsureValidation()
+        {
+
+        }
+    }
+
+}
