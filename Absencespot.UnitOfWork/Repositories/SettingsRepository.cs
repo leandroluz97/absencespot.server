88036824b4b2afb6eabd329e52f726ffc2031ef9
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-28 00:50:18 +0000

    added FindByCompanyIdAsync settings method

--- Absencespot.UnitOfWork/Repositories/SettingsRepository.cs
+++ Absencespot.UnitOfWork/Repositories/SettingsRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Absencespot.Domain;
 using Absencespot.Infrastructure.Abstractions.Repositories;
 using Absencespot.SqlServer;
+using Absencespot.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,15 @@ namespace Absencespot.UnitOfWork.Repositories
     {
         public SettingsRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
+        }
+
+        public async Task<Settings>? FindByCompanyIdAsync(int companyId, RepositoryOptions? options = null, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Settings> source = AsQueryable(options);
+            source = source.Where(s => s.Id == companyId);
+            var result = await FirstOrDefaultAsync(source, cancellationToken)
+                .ConfigureAwait(false);
+            return result;
         }
     }
 }
