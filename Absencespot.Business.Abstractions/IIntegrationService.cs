6a4c28d9f35f24bb3968b5ad986703224e3039d8
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-29 00:51:23 +0000

    added integration service interface

---
+++ Absencespot.Business.Abstractions/IIntegrationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface IIntegrationService
+    {
+        Task<IEnumerable<Dtos.Integration>> GetAllAsync(Guid companyId, CancellationToken cancellationToken = default);
+        Task<Dtos.Integration> UpdateAsync(Guid companyId, Dtos.Integration integration, CancellationToken cancellationToken = default);
+    }
+}
