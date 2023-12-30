8d3a40499a3890bf8b39ba374ae18aba5c3d2763
leandroluz97 <leandrodaluz97@gmail.com>
2023-12-30 04:33:15 +0000

    added integration dtos

---
+++ Absencespot.Dtos/Integration.cs
@@ -0,0 +1,16 @@
+﻿using Absencespot.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class Integration
+    {
+        public ProviderType Provider { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
