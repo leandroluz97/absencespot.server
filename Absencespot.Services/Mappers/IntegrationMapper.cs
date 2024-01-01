53e34c3370e89d1d456c222c98f27968c0f0f30b
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-01 20:25:19 +0000

    added integration DTO

---
+++ Absencespot.Services/Mappers/IntegrationMapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class IntegrationMapper
+    {
+        public static Domain.Integration ToDomain(Dtos.Integration dto)
+        {
+            return new Domain.Integration() 
+            {
+                IsActive = dto.IsActive,
+                Provider = dto.Provider,
+            };
+        }
+        public static Dtos.Integration ToDto(Domain.Integration domain)
+        {
+            return new Dtos.Integration() 
+            {
+                IsActive = domain.IsActive,
+                Provider = domain.Provider,
+            };
+        }
+    }
+}
