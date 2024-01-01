a6fc63ec3ff29b89c48bce20fb8163090561465f
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-01 20:27:16 +0000

    added settings mapper DTO

---
+++ Absencespot.Services/Mappers/SettingsMapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Services.Mappers
+{
+    public class SettingsMapper
+    {
+        public static Domain.Settings ToDomain(Dtos.Settings dto)
+        {
+            return new Domain.Settings() 
+            { 
+                DateFormat = dto.DateFormat,
+                DisplayBirthday = dto.DisplayBirthday,
+                DisplayMode = dto.DisplayMode,
+                IsLoginFromEmailLinkRequired = dto.IsLoginFromEmailLinkRequired,
+                OnBehalfOf = dto.OnBehalfOf,
+                PrivacyMode = dto.PrivacyMode,
+                TimeFormat = dto.TimeFormat,
+                TimeZone = dto.TimeZone,
+                FirstDayOfTheWeek = dto.FirstDayOfTheWeek,
+            };
+        }
+        public static Dtos.Settings ToDto(Domain.Settings domain)
+        {
+            return new Dtos.Settings() 
+            {
+                DateFormat = domain.DateFormat,
+                DisplayBirthday = domain.DisplayBirthday,
+                DisplayMode = domain.DisplayMode,
+                IsLoginFromEmailLinkRequired = domain.IsLoginFromEmailLinkRequired,
+                OnBehalfOf = domain.OnBehalfOf,
+                PrivacyMode = domain.PrivacyMode,
+                TimeFormat = domain.TimeFormat,
+                TimeZone = domain.TimeZone,
+                FirstDayOfTheWeek = domain.FirstDayOfTheWeek,
+            };
+        }
+    }
+}
