3f58ed6ab5e6feeaad90920ba2a99018f7bb768d
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-01 20:27:45 +0000

    added trackrecord mapper

--- Absencespot.Services/Mappers/TrackRecordMapper.cs
+++ Absencespot.Services/Mappers/TrackRecordMapper.cs
@@ -10,11 +10,31 @@ namespace Absencespot.Services.Mappers
     {
         public static Domain.TrackRecord ToDomain(Dtos.TrackRecord dto)
         {
-            return new Domain.TrackRecord() { };
+            return new Domain.TrackRecord() 
+            {
+                Date = dto.Date,
+                CheckIn = dto.CheckIn,
+                CheckOut = dto.CheckOut,
+                BreakStart = dto.BreakStart,
+                BreakEnd = dto.BreakEnd,
+                Location = dto.Location,
+                Note = dto.Note,
+            };
         }
         public static Dtos.TrackRecord ToDto(Domain.TrackRecord domain)
         {
-            return new Dtos.TrackRecord() { };
+            return new Dtos.TrackRecord() 
+            {
+                Id = domain.GlobalId,
+                Date = domain.Date,
+                CheckIn = domain.CheckIn,
+                CheckOut = domain.CheckOut,
+                BreakStart = domain.BreakStart, 
+                BreakEnd = domain.BreakEnd, 
+                Location = domain.Location,
+                Note = domain.Note,
+            };
         }
     }
 }
+
