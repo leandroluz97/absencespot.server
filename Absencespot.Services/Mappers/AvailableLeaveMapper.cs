ecb78b469f5098e6370fc37f29b73026bba163fa
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-04 23:26:56 +0000

    added availableLeave mapper properties

--- Absencespot.Services/Mappers/AvailableLeaveMapper.cs
+++ Absencespot.Services/Mappers/AvailableLeaveMapper.cs
@@ -10,11 +10,21 @@ namespace Absencespot.Services.Mappers
     {
         public static Domain.AvailableLeave ToDomain(Dtos.AvailableLeave dto)
         {
-            return new Domain.AvailableLeave() { };
+            return new Domain.AvailableLeave() 
+            { 
+                AvailableDays = dto.AvailableDays,
+                Period = dto.Period,
+            };
         }
         public static Dtos.AvailableLeave ToDto(Domain.AvailableLeave domain)
         {
-            return new Dtos.AvailableLeave() { };
+            return new Dtos.AvailableLeave() 
+            { 
+                AvailableDays = domain.AvailableDays,
+                Period = domain.Period,
+                AbsenceId = domain.Absence.GlobalId,
+                LeaveId = domain.Absence.Leave.GlobalId,
+            };
         }
     }
 }
--- Absencespot.Services/Mappers/RequestMapper.cs
+++ Absencespot.Services/Mappers/RequestMapper.cs
@@ -10,11 +10,28 @@ namespace Absencespot.Services.Mappers
     {
         public static Domain.Request ToDomain(Dtos.Request dto)
         {
-            return new Domain.Request() { };
+            return new Domain.Request() 
+            { 
+                EndDate = dto.EndDate,
+                StartDate = dto.StartDate,
+                Note = dto.Note,
+                Status =  dto.Status,
+            };
         }
         public static Dtos.Request ToDto(Domain.Request domain)
         {
-            return new Dtos.Request() { };
+            return new Dtos.Request() 
+            { 
+                StartDate = domain.StartDate,
+                EndDate = domain.EndDate,
+                Note = domain.Note,
+                Status = domain.Status,
+                LeaveId = domain.Leave.GlobalId,
+                ApproverId = domain.Approver.GlobalId,
+                OnBehalfOfId = domain.OnBehalfOf.GlobalId,
+                UserId = domain.User.GlobalId,
+                OfficeId = domain.User.Office.GlobalId,
+            };
         }
     }
 }
