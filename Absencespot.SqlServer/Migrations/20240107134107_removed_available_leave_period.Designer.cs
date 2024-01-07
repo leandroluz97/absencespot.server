6f9e3db697c40774afb8b3892266c1ecc3b600b8
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-07 18:47:39 +0000

    added start and end Date to AvailableLeave migration and database update

--- Absencespot.SqlServer/Migrations/20240107134107_removed_available_leave_period.Designer.cs
+++ Absencespot.SqlServer/Migrations/20240107134107_removed_available_leave_period.Designer.cs
@@ -4,6 +4,7 @@ using Absencespot.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Absencespot.SqlServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240107134107_removed_available_leave_period")]
+    partial class removed_available_leave_period
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Absencespot.SqlServer/Migrations/20240107134107_removed_available_leave_period.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Absencespot.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class removed_available_leave_period : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Period",
+                table: "AvailableLeave",
+                newName: "StartDate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "AvailableLeave",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "AvailableLeave");
+
+            migrationBuilder.RenameColumn(
+                name: "StartDate",
+                table: "AvailableLeave",
+                newName: "Period");
+        }
+    }
+}
