4c5f0fd77ab3f83215acc546a84d366c0cd268ec
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-07 01:33:22 +0000

    update rejectRequest DTO

--- Absencespot.Dtos/Request/RejectRequest.cs
+++ Absencespot.Dtos/Request/RejectRequest.cs
@@ -9,16 +9,15 @@ namespace Absencespot.Dtos
 {
     public class RejectRequest
     {
-        public StatusType Status { get; set; }
         public Guid ApproverId { get; set; }
         public string Reason { get; set; }
 
         public void EnsureValidation()
         {
-            if (Status != StatusType.Rejected)
-            {
-                throw new ArgumentException(nameof(Status));
-            }
+            //if (Status != StatusType.Rejected)
+            //{
+            //    throw new ArgumentException(nameof(Status));
+            //}
             if (ApproverId == default)
             {
                 throw new ArgumentException(nameof(ApproverId));
