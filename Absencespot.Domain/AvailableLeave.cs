e273b3d2cb0e547d0d212fc2c88be0d67fd87b73
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-08 12:25:03 +0000

    replaced period for start and end date in AvailableLeave

--- Absencespot.Domain/AvailableLeave.cs
+++ Absencespot.Domain/AvailableLeave.cs
@@ -5,7 +5,9 @@ namespace Absencespot.Domain
     public class AvailableLeave : Entity
     {
         public double AvailableDays { get; set; }
-        public DateTime Period { get; set; }
+        //public DateTime Period { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public int? AbsenceId { get; set; }
         public Absence? Absence { get; set; }
         public int? UserId { get; set; }
