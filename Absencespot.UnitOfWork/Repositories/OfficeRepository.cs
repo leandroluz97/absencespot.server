4c60e13457cba249fcc5dbf9f9185a08e64cf81b
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-14 21:42:26 +0000

    added FindByIdIncludedAsync into OfficeRepository

--- Absencespot.UnitOfWork/Repositories/OfficeRepository.cs
+++ Absencespot.UnitOfWork/Repositories/OfficeRepository.cs
@@ -28,5 +28,17 @@ namespace Absencespot.UnitOfWork.Repositories
             var result = await FirstOrDefaultAsync(source, cancellationToken).ConfigureAwait(false);
             return result;
         }
+        public async Task<Office?> FindByIdIncludedAsync(int id, RepositoryOptions? options = null, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Office> source = AsQueryable(options);
+            source = source.Where(s => s.Id == id);
+            source = Include(source, s => s.Address);
+            source = Include(source, s => s.Absences);
+            source = IncludeThen<Domain.Absence, Domain.Leave>(source, a => a.Leave);
+            source = Include(source, s => s.AvailableLeaves);
+            source = IncludeThen<Domain.OfficeLeave, Domain.Leave>(source, s => s.Leave);
+            var result = await FirstOrDefaultAsync(source, cancellationToken).ConfigureAwait(false);
+            return result;
+        }
     }
 }
