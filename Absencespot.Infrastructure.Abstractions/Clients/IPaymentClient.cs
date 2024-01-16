65f452ac1c00139c192b360e8e38bd3efb9b89ff
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-16 23:45:54 +0000

    added IPaymentClient abstraction

---
+++ Absencespot.Infrastructure.Abstractions/Clients/IPaymentClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Infrastructure.Abstractions.Clients
+{
+    public interface IPaymentClient
+    {
+        public void CreateSubscription(int seat);
+    }
+}
