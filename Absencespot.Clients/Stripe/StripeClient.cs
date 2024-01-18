d67e060ba13932b79a41b21cbc6bec44ad764d58
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-18 23:50:35 +0000

    added stripe client

---
+++ Absencespot.Clients/Stripe/StripeClient.cs
@@ -0,0 +1,43 @@
+﻿using Absencespot.Infrastructure.Abstractions.Clients;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Clients.Stripe
+{
+    public class StripeClient: IPaymentClient
+    {
+        public StripeClient()
+        {
+
+        }
+
+        public void CreateSubscription(int seat)
+        {
+            var options = new SubscriptionCreateOptions
+            {
+                Customer = "cus_4fdAW5ftNQow1a",
+                Items = new List<SubscriptionItemOptions>
+                {
+                    new SubscriptionItemOptions { Price = "price_1OZNFpA0D3nIAUzqndEcS9Mj", Quantity = seat },
+                },
+            };
+            var service = new SubscriptionService();
+            service.Create(options);
+            //var options = new PriceCreateOptions
+            //{
+            //    Nickname = "Standard Cost Per 5 Users",
+            //    TransformQuantity = new PriceTransformQuantityOptions { DivideBy = 5, Round = "up" },
+            //    UnitAmount = 1000,
+            //    Currency = "usd",
+            //    Recurring = new PriceRecurringOptions { Interval = "month", UsageType = "licensed" },
+            //    Product = "{{PRODUCTIVITY_SUITE_ID}}",
+            //};
+            //var service = new PriceService();
+            //service.Create(options);
+        }
+    }
+}
