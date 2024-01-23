08e7d6a29c9ea5be63ba8f782faabd559b10699f
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-23 00:53:21 +0000

    added IStripeClient

--- Absencespot.Infrastructure.Abstractions/Clients/IStripeClient.cs
+++ Absencespot.Infrastructure.Abstractions/Clients/IStripeClient.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Absencespot.Dtos;
 using Stripe;
 
 
 namespace Absencespot.Infrastructure.Abstractions.Clients
 {
-    public interface IStripeClient : ISubscriptionClient<Stripe.Subscription>
+    public interface IStripeClient : ISubscriptionClient<Stripe.Subscription, CreateSubscription>
     {
-        Task<Dtos.ResponsePublishableKey> GetPublishableKeyAsync(CancellationToken cancellationToken = default);
-        Task<Stripe.Customer> CreateCustomerAsync<D>(Guid companyId, Dtos.Customer customer, CancellationToken cancellationToken = default);
+        public string GetPublishableKeyAsync(CancellationToken cancellationToken = default);
+        Task<Stripe.Customer> CreateCustomerAsync(Guid companyId, Dtos.Customer customer, CancellationToken cancellationToken = default);
         Task<IEnumerable<Stripe.Price>> GetPricesAsync(CancellationToken cancellationToken = default);
         Task Events<D>(D intent, CancellationToken cancellationToken = default);    
     }
