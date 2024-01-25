f52f3f0c2fc419305e96c2431abcfeae6fa84a5c
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-25 23:50:53 +0000

    update ISubscriptionService interface

--- Absencespot.Business.Abstractions/ISubscriptionService.cs
+++ Absencespot.Business.Abstractions/ISubscriptionService.cs
@@ -9,12 +9,12 @@ namespace Absencespot.Business.Abstractions
     public interface ISubscriptionService
     {
         Task<Dtos.ResponsePublishableKey> GetPublishableKeyAsync(CancellationToken cancellationToken = default);
-        Task CreateAsync(Guid companyId, Dtos.CreateSubscription subscription, CancellationToken cancellationToken = default);
+        Task<Dtos.ResponseSubscription> CreateAsync(Guid companyId, Dtos.CreateSubscription subscription, CancellationToken cancellationToken = default);
         Task CancelAsync(Guid companyId, string customerId, CancellationToken cancellationToken = default);
         Task<Dtos.ResponseSubscription> GetAsync(Guid companyId, string customerId, CancellationToken cancellationToken = default);
         Task<Dtos.Customer> CreateCustomerAsync(Guid companyId, Dtos.Customer customer, CancellationToken cancellationToken = default);
         Task<IEnumerable<Dtos.Price>> GetPricesAsync(CancellationToken cancellationToken = default);
-        Task<Dtos.ResponsePaymentIntent> CreatePaymentIntentAsync(Guid companyId, Dtos.CreatePaymentIntent intent, CancellationToken cancellationToken = default);
+        //<Dtos.ResponsePaymentIntent> CreatePaymentIntentAsync(Guid companyId, Dtos.CreatePaymentIntent intent, CancellationToken cancellationToken = default);
         Task Events(Dtos.CreatePaymentIntent intent, CancellationToken cancellationToken = default);
     }
 }
