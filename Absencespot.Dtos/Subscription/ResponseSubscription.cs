18577ffe0faf853cd84427b30e7353b0b4ae58a4
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-28 22:19:12 +0000

    updatr Response Subscription DTO

--- Absencespot.Dtos/Subscription/ResponseSubscription.cs
+++ Absencespot.Dtos/Subscription/ResponseSubscription.cs
@@ -3,5 +3,9 @@
     public class ResponseSubscription
     {
         public string Id { get; set; }
+        public string ClientSecret { get; set; }
+        public IEnumerable<string> Ids { get; set; }
+
+        
     }
 }
