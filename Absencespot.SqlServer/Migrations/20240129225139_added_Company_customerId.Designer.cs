438b2ea89d43574680dad615d3cee15b21170b44
leandroluz97 <leandrodaluz97@gmail.com>
2024-01-31 23:58:41 +0000

    added customer to company domain migrations

--- Absencespot.SqlServer/Migrations/20240129225139_added_Company_customerId.Designer.cs
+++ Absencespot.SqlServer/Migrations/20240129225139_added_Company_customerId.Designer.cs
@@ -4,6 +4,7 @@ using Absencespot.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Absencespot.SqlServer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240129225139_added_Company_customerId")]
+    partial class added_Company_customerId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Absencespot.SqlServer/Migrations/20240129225139_added_Company_customerId.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Absencespot.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_Company_customerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalWorkDays",
+                table: "WorkSchedule",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Hours",
+                table: "WorkSchedule",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "customerId",
+                table: "Company",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "customerId",
+                table: "Company");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TotalWorkDays",
+                table: "WorkSchedule",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Hours",
+                table: "WorkSchedule",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+    }
+}
