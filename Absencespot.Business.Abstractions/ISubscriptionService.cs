6f2a8fd13329e70793020a542bd55d721a72f6a7
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-01 22:58:09 +0000

    added update and upgrade to ISubscriptionService

--- Absencespot.Business.Abstractions/ISubscriptionService.cs
+++ Absencespot.Business.Abstractions/ISubscriptionService.cs
@@ -10,10 +10,14 @@ namespace Absencespot.Business.Abstractions
     {
         Task<Dtos.ResponsePublishableKey> GetPublishableKeyAsync(CancellationToken cancellationToken = default);
         Task<Dtos.ResponseSubscription> CreateAsync(Guid companyId, Dtos.CreateSubscription subscription, CancellationToken cancellationToken = default);
+        Task UpdateAsync(Guid companyId, Dtos.UpdateSubscription subscription, CancellationToken cancellationToken = default);
+        Task<Dtos.ResponseSubscription> UpgradeAsync(Guid companyId, Dtos.UpgradeSubscription subscription, CancellationToken cancellationToken = default);
         Task CancelAsync(Guid companyId, string customerId, CancellationToken cancellationToken = default);
         Task<Dtos.ResponseSubscription> GetAsync(Guid companyId, string customerId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Dtos.ResponseSubscription>> GetAllAsync(Guid companyId, string customerId, string priceId, CancellationToken cancellationToken = default);
         Task<Dtos.Customer> CreateCustomerAsync(Guid companyId, Dtos.Customer customer, CancellationToken cancellationToken = default);
         Task<IEnumerable<Dtos.Price>> GetPricesAsync(CancellationToken cancellationToken = default);
+        Task<IEnumerable<Dtos.ResponsePaymentIntent>> GetPaymentIntentsAsync(Guid companyId, CancellationToken cancellationToken = default);
         //<Dtos.ResponsePaymentIntent> CreatePaymentIntentAsync(Guid companyId, Dtos.CreatePaymentIntent intent, CancellationToken cancellationToken = default);
         Task Events(Dtos.CreatePaymentIntent intent, CancellationToken cancellationToken = default);
     }
