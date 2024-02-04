5d71e57401ff52478bbe339fd9d0046ee203e901
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-04 15:37:06 +0000

    added ResponsePaymentMethod DTO

---
+++ Absencespot.Dtos/Subscription/ResponsePaymentMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace Absencespot.Dtos
+{
+    public class ResponsePaymentMethod
+    {
+        public string Id { get; set; }
+        public string Brand { get; set; }
+        public bool IsExpired { get; set; }
+        public string Number { get; set; }
+    }
+}
