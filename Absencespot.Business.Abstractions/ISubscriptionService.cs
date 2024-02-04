3f98f8b873a83589bc802660d8be36459b71c963
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-04 15:34:06 +0000

    added GetPaymentMethodAsync service method interface

--- Absencespot.Business.Abstractions/ISubscriptionService.cs
+++ Absencespot.Business.Abstractions/ISubscriptionService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stripe;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +10,17 @@ namespace Absencespot.Business.Abstractions
     public interface ISubscriptionService
     {
         Task<Dtos.ResponsePublishableKey> GetPublishableKeyAsync(CancellationToken cancellationToken = default);
+        Task<Dtos.ResponsePaymentMethod> GetPaymentMethodAsync(Guid companyId, CancellationToken cancellationToken = default);
         Task<Dtos.ResponseSubscription> CreateAsync(Guid companyId, Dtos.CreateSubscription subscription, CancellationToken cancellationToken = default);
         Task UpdateAsync(Guid companyId, Dtos.UpdateSubscription subscription, CancellationToken cancellationToken = default);
         Task<Dtos.ResponseSubscription> UpgradeAsync(Guid companyId, Dtos.UpgradeSubscription subscription, CancellationToken cancellationToken = default);
         Task CancelAsync(Guid companyId, string customerId, CancellationToken cancellationToken = default);
-        Task<Dtos.ResponseSubscription> GetAsync(Guid companyId, string customerId, CancellationToken cancellationToken = default);
-        Task<IEnumerable<Dtos.ResponseSubscription>> GetAllAsync(Guid companyId, string customerId, string priceId, CancellationToken cancellationToken = default);
+        Task<Dtos.ResponseSubscription> GetAsync(Guid companyId, string subscriptionId, CancellationToken cancellationToken = default);
+        Task<IEnumerable<Dtos.ResponseSubscription>> GetAllAsync(Guid companyId, CancellationToken cancellationToken = default);
         Task<Dtos.Customer> CreateCustomerAsync(Guid companyId, Dtos.Customer customer, CancellationToken cancellationToken = default);
         Task<IEnumerable<Dtos.Price>> GetPricesAsync(CancellationToken cancellationToken = default);
         Task<IEnumerable<Dtos.Invoice>> GetInvoicesAsync(Guid companyId, CancellationToken cancellationToken = default);
         Task<IEnumerable<Dtos.ResponsePaymentIntent>> GetPaymentIntentsAsync(Guid companyId, CancellationToken cancellationToken = default);
-        Task Events(Dtos.CreatePaymentIntent intent, CancellationToken cancellationToken = default);
+        Task Events(Stripe.Event events, CancellationToken cancellationToken = default);
     }
 }
