70427b6013a0ca98a97210ae1d33a25ac2997c3f
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-08 02:26:14 +0000

    added Payment method and SetupIntent DTOs

---
+++ Absencespot.Dtos/Subscription/AttachPaymentMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class AttachPaymentMethod
+    {
+        public string PaymentMethodId { get; set; }
+    }
+}
--- Absencespot.Dtos/Subscription/ResponseSubscription.cs
+++ Absencespot.Dtos/Subscription/ResponseSubscription.cs
@@ -3,12 +3,12 @@
     public class ResponseSubscription
     {
         public string Id { get; set; }
-        public string ClientSecret { get; set; }
+        public string? ClientSecret { get; set; }
         public string PriceId { get; set; }
         public string? PaymentMethodId { get; set; }
         public DateTime? CanceledAt { get; set; }
         public string Status  { get; set; }
-        public IEnumerable<string> Ids { get; set; }
+        public IEnumerable<string>? Ids { get; set; }
 
         
     }
