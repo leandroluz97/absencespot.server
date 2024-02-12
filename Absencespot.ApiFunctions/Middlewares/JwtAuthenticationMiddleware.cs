c07125ba970bef24ac1c62a746ba76064cc816c3
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-12 01:12:09 +0000

    added JwtAuthenticationMiddleware to handle token validation

---
+++ Absencespot.ApiFunctions/Middlewares/JwtAuthenticationMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.ApiFunctions.Middlewares
+{
+    public class JwtAuthenticationMiddleware : IFunctionsWorkerMiddleware
+    {
+        public Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
