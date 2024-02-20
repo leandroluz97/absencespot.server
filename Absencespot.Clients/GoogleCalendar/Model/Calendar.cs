b4eeeeea6d8512156f1f0195248af41f0914e90a
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-20 23:44:47 +0000

    added calendar model

---
+++ Absencespot.Clients/GoogleCalendar/Model/Calendar.cs
@@ -0,0 +1,13 @@
+﻿using Absencespot.Infrastructure.Abstractions.Clients.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Clients.GoogleCalendar.Model
+{
+    public class Calendar : BaseEntity
+    {
+    }
+}
