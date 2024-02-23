deed631c71746ccaa7f75ba5e813a3a8313cd9c9
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-23 00:26:58 +0000

    added google calendar options

---
+++ Absencespot.Clients/GoogleCalendar/Options/GoogleAuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Clients.GoogleCalendar.Options
+{
+    public class GoogleAuthOptions
+    {
+        public string ClientId { get; set; } = null!;
+        public string ClientSecret { get; set; } = null!;
+        public string Scopes { get; set; } = null!;
+        public string User { get; set; } = null!;
+        public string ApplicationName { get; set; } = null!;
+    }
+}
