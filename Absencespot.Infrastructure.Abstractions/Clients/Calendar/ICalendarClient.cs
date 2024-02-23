9a600bcbdb5e2fb5381e0a09e44000dacf39b6ba
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-23 00:40:35 +0000

    added ICalendarClient abstraction

---
+++ Absencespot.Infrastructure.Abstractions/Clients/Calendar/ICalendarClient.cs
@@ -0,0 +1,17 @@
+﻿using Google.Apis.Calendar.v3.Data;
+
+namespace Absencespot.Infrastructure.Abstractions.Clients.Calendar
+{
+    public interface ICalendarClient
+    {
+        Task<CalendarListEntry> CreateAsync(CalendarListEntry options, CancellationToken cancellationToken = default);
+        Task<CalendarListEntry> GetById(string calendarId, CancellationToken cancellationToken = default);
+        Task<CalendarListEntry> GetByOwnerId(string calendarId, CancellationToken cancellationToken = default);
+        Task<CalendarList> GetListAsync(CancellationToken cancellationToken = default);
+        Task<CalendarListEntry> UpdateAsync(string calendarId, CalendarListEntry options, CancellationToken cancellationToken = default);
+        Task DeleteAsync(string calendarId, CancellationToken cancellationToken = default);
+        Task<Event> CreateEventAsync(string calendarId, Event options, CancellationToken cancellationToken = default);
+        Task<Event> UpdateEventAsync(string calendarId, string eventId, Event options, CancellationToken cancellationToken = default);
+        Task DeleteEventAsync(string calendarId, string eventId, CancellationToken cancellationToken = default);
+    }
+}
