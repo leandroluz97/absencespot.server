9072e069edb2572a11a1c72e66b9a19106ce07f4
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-24 16:52:12 +0000

    added calendar SQL server migrations

---
+++ Absencespot.SqlServer/Migrations/20240224164327_added_calendarId.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Absencespot.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_calendarId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CalendarId",
+                table: "Team",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CalendarId",
+                table: "Office",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CalendarId",
+                table: "Company",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CalendarId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CalendarId",
+                table: "Team");
+
+            migrationBuilder.DropColumn(
+                name: "CalendarId",
+                table: "Office");
+
+            migrationBuilder.DropColumn(
+                name: "CalendarId",
+                table: "Company");
+
+            migrationBuilder.DropColumn(
+                name: "CalendarId",
+                table: "AspNetUsers");
+        }
+    }
+}
