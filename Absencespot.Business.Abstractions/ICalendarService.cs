ecc8ff6738b88ebe8116112407841ed331dd9a86
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-29 00:40:22 +0000

    added IServiceCalendar interface

---
+++ Absencespot.Business.Abstractions/ICalendarService.cs
@@ -0,0 +1,12 @@
+﻿using Absencespot.Dtos;
+using Google.Apis.Calendar.v3.Data;
+
+namespace Absencespot.Business.Abstractions
+{
+    public interface ICalendarService
+    {
+        Task<CalendarFeed> GetUrlsAsync(Guid companyId, Guid userId, CancellationToken cancellationToken = default);
+        Task<CalendarListEntry> GetContent(string id, CancellationToken cancellationToken = default);
+        Task<Events> GetEventsContent(string id, CancellationToken cancellationToken = default);
+    }
+}
