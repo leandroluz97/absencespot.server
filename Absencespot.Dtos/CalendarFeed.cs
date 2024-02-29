6859599679c8a54dc68897a196ef901354156742
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-29 00:45:48 +0000

    added CalendarFeed DTO

---
+++ Absencespot.Dtos/CalendarFeed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class CalendarFeed
+    {   
+        public string Company { get; set; }
+        public string Office { get; set; }
+        public string Team { get; set; }
+        public string User { get; set; }
+    }
+}
