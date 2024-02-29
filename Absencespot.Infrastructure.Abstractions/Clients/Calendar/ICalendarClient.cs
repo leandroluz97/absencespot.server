6683c4ccc5a8ee084bb64044a06394b9e5c8703e
leandroluz97 <leandrodaluz97@gmail.com>
2024-02-29 00:48:54 +0000

    update ICalendarClient

--- Absencespot.Infrastructure.Abstractions/Clients/Calendar/ICalendarClient.cs
+++ Absencespot.Infrastructure.Abstractions/Clients/Calendar/ICalendarClient.cs
@@ -4,16 +4,16 @@ namespace Absencespot.Infrastructure.Abstractions.Clients.Calendar
 {
     public interface ICalendarClient
     {
-
         Task<Google.Apis.Calendar.v3.Data.Calendar> CreateAsync(Google.Apis.Calendar.v3.Data.Calendar options, CancellationToken cancellationToken = default);
         Task<CalendarListEntry> GetById(string calendarId, CancellationToken cancellationToken = default);
-        Task<Events?> GetHolidays(string country, CancellationToken cancellationToken = default);
-        Task<CalendarListEntry> GetByOwnerId(string calendarId, CancellationToken cancellationToken = default);
-        Task<CalendarList> GetListAsync(CancellationToken cancellationToken = default);
         Task<CalendarListEntry> UpdateAsync(string calendarId, CalendarListEntry options, CancellationToken cancellationToken = default);
         Task DeleteAsync(string calendarId, CancellationToken cancellationToken = default);
+        Task<Google.Apis.Calendar.v3.Data.Calendar> GetByOwnerId(string calendarId, CancellationToken cancellationToken = default);
+        Task<CalendarList> GetListAsync(CancellationToken cancellationToken = default);
+        Task<Events> GetEventListAsync(string calendarId, CancellationToken cancellationToken = default);
         Task<Event> CreateEventAsync(string calendarId, Event options, CancellationToken cancellationToken = default);
         Task<Event> UpdateEventAsync(string calendarId, string eventId, Event options, CancellationToken cancellationToken = default);
         Task DeleteEventAsync(string calendarId, string eventId, CancellationToken cancellationToken = default);
+        Task<Events?> GetHolidays(string country, CancellationToken cancellationToken = default);
     }
 }
