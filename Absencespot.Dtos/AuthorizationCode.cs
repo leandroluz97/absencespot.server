2acb527dd2ce5c1015cdef72dfa024cf815a7ad3
leandroluz97 <leandrodaluz97@gmail.com>
2024-03-05 02:28:19 +0000

    added AuthorizationCode Dto

---
+++ Absencespot.Dtos/AuthorizationCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class AuthorizationCode
+    {
+        public string Code { get; set; }
+        public string? State { get; set; }
+
+        public void EnsureValidation()
+        {
+            if (string.IsNullOrWhiteSpace(Code))
+            {
+                throw new ArgumentException("Code is required.", nameof(Code));
+            }
+        }
+    }
+}
