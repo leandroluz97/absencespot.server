7ab61643b3374c3879c55a9dd7264006bbd11f09
leandroluz97 <leandrodaluz97@gmail.com>
2024-03-10 02:38:25 +0000

    added MicrosoftAuthOptions DTO

---
+++ Absencespot.Dtos/MicrosoftAuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Absencespot.Dtos
+{
+    public class MicrosoftAuthOptions
+    {
+        public string ClientId { get; set; } = null!;
+        public string ClientSecret { get; set; } = null!;
+        public string Scopes { get; set; } = null!;
+        public string RedirectUri { get; set; } = null!;
+        public string AuthorityUri { get; set; } = null!;
+    }
+}
