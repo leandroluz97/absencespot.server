8dfa6c966ffc0fbb82a35b621fb9a0f5bec6891a
leandroluz97 <leandrodaluz97@gmail.com>
2024-03-12 23:52:23 +0000

    updated Microsoft auth options

--- Absencespot.Dtos/MicrosoftAuthOptions.cs
+++ Absencespot.Dtos/MicrosoftAuthOptions.cs
@@ -13,5 +13,12 @@ namespace Absencespot.Dtos
         public string Scopes { get; set; } = null!;
         public string RedirectUri { get; set; } = null!;
         public string AuthorityUri { get; set; } = null!;
+
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public bool ValidateIssuer { get; set; }
+        public bool ValidateAudience { get; set; }
+        public bool ValidateLifetime { get; set; }
+        public bool ValidateIssuerSigningKey { get; set; }
     }
 }
